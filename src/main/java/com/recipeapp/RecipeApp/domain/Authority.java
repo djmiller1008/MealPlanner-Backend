8ea435c92cf200da69bc1776556d4b9cd725b3b1
David <djmiller1008@yahoo.com>
2023-02-28 22:19:01 +0800

    Move domain package into main package, add authority class

---
+++ src/main/java/com/recipeapp/RecipeApp/domain/Authority.java
@@ -0,0 +1,87 @@
+package com.recipeapp.RecipeApp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+public class Authority implements GrantedAuthority {
+    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    @ManyToOne
+    private User user;
+
+
+    public Authority() {
+    }
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    public Authority(Long id, String authority, User user) {
+        this.id = id;
+        this.authority = authority;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Authority id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public Authority authority(String authority) {
+        setAuthority(authority);
+        return this;
+    }
+
+    public Authority user(User user) {
+        setUser(user);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", authority='" + getAuthority() + "'" +
+            ", user='" + getUser() + "'" +
+            "}";
+    }
+
+
+
+
+}
---
+++ src/main/java/com/recipeapp/RecipeApp/domain/User.java
@@ -0,0 +1,90 @@
+package com.recipeapp.RecipeApp.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collection;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+
+
+
+@Entity
+@Table(name="users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    
+    @OneToMany
+    private List<UserRecipe> recipes = new ArrayList<>();
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<UserRecipe> getRecipes() {
+        return this.recipes;
+    }
+
+    public void setRecipes(List<UserRecipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new Authority("ROLE_USER"));
+        return roles;
+    }
+    
+}
