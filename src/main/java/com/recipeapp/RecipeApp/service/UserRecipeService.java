0c649155380bc95d05672478ee5d1116ddd60b3d
David <djmiller1008@yahoo.com>
2023-03-04 22:55:37 +0800

    Add User Recipe Service, add create user recipe method

---
+++ src/main/java/com/recipeapp/RecipeApp/service/UserRecipeService.java
@@ -0,0 +1,23 @@
+package com.recipeapp.RecipeApp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.recipeapp.RecipeApp.domain.UserRecipe;
+import com.recipeapp.RecipeApp.domain.User;
+import com.recipeapp.RecipeApp.repository.UserRecipeRepository;
+
+@Service
+public class UserRecipeService {
+
+    @Autowired
+    private UserRecipeRepository userRecipeRepository;
+    
+    public UserRecipe createUserRecipe(User user, String name) {
+        UserRecipe userRecipe = new UserRecipe();
+        userRecipe.setName(name);
+        userRecipe.setUser(user);
+
+        return userRecipeRepository.save(userRecipe);
+    }
+}
