8e24915bc06188532153ef7aa9abfec8715dad94
David <djmiller1008@yahoo.com>
2023-03-05 00:06:38 +0800

    Add ManyToOne relationship with User

--- src/main/java/com/recipeapp/RecipeApp/domain/UserRecipe.java
+++ src/main/java/com/recipeapp/RecipeApp/domain/UserRecipe.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity 
 public class UserRecipe {
@@ -12,6 +13,8 @@ public class UserRecipe {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToOne
     private User user;
 
     public Long getId() {
