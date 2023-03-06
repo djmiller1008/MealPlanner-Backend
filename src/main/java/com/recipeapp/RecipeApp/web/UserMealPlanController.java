14cf3a0c7c0b424b1a98445bb99df4de669e7b93
David <djmiller1008@yahoo.com>
2023-03-06 10:51:30 +0800

    Add UserMealPlanController with post endpoint for creating user meal plans

---
+++ src/main/java/com/recipeapp/RecipeApp/web/UserMealPlanController.java
@@ -0,0 +1,27 @@
+package com.recipeapp.RecipeApp.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.recipeapp.RecipeApp.service.UserMealPlanService;
+import com.recipeapp.RecipeApp.domain.User;
+import com.recipeapp.RecipeApp.domain.UserMealPlan;
+
+@RestController
+@RequestMapping("/api/user-mealplans")
+public class UserMealPlanController {
+    
+    @Autowired
+    private UserMealPlanService userMealPlanService;
+
+    @PostMapping("")
+    public ResponseEntity<?> createUserMealPlan(@AuthenticationPrincipal User user, @RequestBody String name) {
+        UserMealPlan newUserMealPlan = userMealPlanService.createUserMealPlan(user, name);
+        return ResponseEntity.ok(newUserMealPlan);
+    }
+}
