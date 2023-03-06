e6c9684912bb4f0d5655da3e6ae4031e7c1db16e
David <djmiller1008@yahoo.com>
2023-03-06 23:22:31 +0800

    Add findByUser repository method

--- src/main/java/com/recipeapp/RecipeApp/repository/UserMealPlanRepository.java
+++ src/main/java/com/recipeapp/RecipeApp/repository/UserMealPlanRepository.java
@@ -1,8 +1,13 @@
 package com.recipeapp.RecipeApp.repository;
 
+import com.recipeapp.RecipeApp.domain.User;
 import com.recipeapp.RecipeApp.domain.UserMealPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
+
 public interface UserMealPlanRepository extends JpaRepository<UserMealPlan, Long> {
     
+    Set<UserMealPlan> findByUser(User user);
 }
