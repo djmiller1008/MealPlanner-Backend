2bdc6ef889d5e5e793895489bb23a955cc99d78e
David <djmiller1008@yahoo.com>
2023-03-06 10:49:57 +0800

    Change UserRecipe to UserMeal

--- src/main/java/com/recipeapp/RecipeApp/repository/UserRecipeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.recipeapp.RecipeApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.recipeapp.RecipeApp.domain.UserRecipe;
-
-public interface UserRecipeRepository extends JpaRepository<UserRecipe, Long> {
-    
-}
