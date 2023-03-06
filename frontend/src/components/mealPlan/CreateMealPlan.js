e852a7d5a8545f1552d5958c2aa065585023cc04
David <djmiller1008@yahoo.com>
2023-03-06 10:40:52 +0800

    Add CreateMealPlan component

---
+++ frontend/src/components/mealPlan/CreateMealPlan.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { createUserMealPlan } from '../util/ApiUtil';
+
+export default function CreateMealPlan() {
+    const [mealPlanName, setMealPlanName] = useState("");
+
+    const handleInput = (e) => {
+      setMealPlanName(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      createUserMealPlan(mealPlanName).then(result => {
+        debugger;
+        console.log(result);
+      })
+    }
+    
+    return (
+      <div>
+          <h1>Enter a Name For Your Meal Plan</h1>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <input onChange={(e) => handleInput(e)} type="text"></input>
+            <input type="submit" value="Create"></input>
+          </form>
+      </div>
+    )
+}
