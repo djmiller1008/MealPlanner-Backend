fe5e094adf442bf267db1b1065d6b262e620fe0f
David <djmiller1008@yahoo.com>
2023-03-09 23:57:39 +0800

    Add functionality to this component, user can choose a meal plan to add a certain meal to

--- frontend/src/components/mealPlan/AddToMealPlan.js
+++ frontend/src/components/mealPlan/AddToMealPlan.js
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { fetchUserMealPlans } from '../util/ApiUtil';
+import { addMealToMealPlan, fetchUserMealPlans } from '../util/ApiUtil';
+import { useLocalState } from '../util/LocalStorageUtil';
 
-export default function AddToMealPlan({ recipeInfo }) {
+export default function AddToMealPlan(props) {
+    const [recipeInfo, setRecipeInfo] = useLocalState(
+                                                    props.location.state.recipeInfo,
+                                                    `recipeInfo${props.location.state.recipeInfo.id}`);
+
+    const [recipeNutritionInfo, setRecipeNutritionInfo] = useLocalState(
+                                                            props.location.state.recipeNutritionInfo,
+                                                            `recipeNutritionInfo${props.location.state.recipeInfo.id}`);
+
+
     const [mealPlans, setMealPlans] = useState(null);
 
     useEffect(() => {
@@ -10,12 +20,50 @@ export default function AddToMealPlan({ recipeInfo }) {
         })
     }, []);
 
+    const handleAddToMealPlan = (e, mealPlanId) => {
+        // TODO add the recipe to a meal plan
+        // Need to pass name, readyInMinutes, servings, ingredients, instructions,
+        // calories, fat, carbs, protein, img url
 
+        //recipeInfoData.title -- name
+        //recipeInfoData.readyInMinutes
+        //recipeInfoData.servings 
+        //recipeInfoData.extendedIngredients.forEach(ingredient => {
+        //  ingredient.name : ingredient.aisle    
+        //})
+
+        //recipeInfoData.instructions -- in html 
+        //All nutrition has g or k at the end, need to trim, store as int
+        //recipeNutritionInfo.calories 
+        //recipeNutritionInfo.carbs
+        //recipeNutritionInfo.fat
+        //recipeNutritionInfo.protein
+
+        const mealData = {};
+        mealData['name'] = recipeInfo.title;
+        mealData['readyInMinutes'] = recipeInfo.readyInMinutes;
+        mealData['servings'] = recipeInfo.servings;
+        mealData['ingredients'] = {};
+        recipeInfo.extendedIngredients.forEach(ingredient => {
+            mealData['ingredients'][ingredient.name] = ingredient.aisle
+        });
+        mealData['mealPlanId'] = mealPlanId;
+
+        addMealToMealPlan(JSON.stringify(mealData));
+    }
+
+  
   return (
     <div>
         <h1>Add To Which Meal Plan?</h1>
+        <h2>Recipe: {recipeInfo.id}</h2>
         <div>
-            {mealPlans ? mealPlans.map((mealPlan, idx) => <div>{mealPlan.name}</div>) : <></>}
+            {mealPlans ? mealPlans.map((mealPlan, idx) => {
+                return (
+                    <div>
+                        <span onClick={e => handleAddToMealPlan(e, mealPlan.id)}>{mealPlan.name}</span>
+                    </div>
+                )}) : <></>}
         </div>
     </div>
   )
