e4862dff84d03f605f18ebf721adf54a8c0461f1
David <djmiller1008@yahoo.com>
2023-03-09 23:50:34 +0800

    Add endpoint for an individual meal plan, return all meals for this mealplan

--- src/main/java/com/recipeapp/RecipeApp/web/UserMealPlanController.java
+++ src/main/java/com/recipeapp/RecipeApp/web/UserMealPlanController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,11 @@ public class UserMealPlanController {
     @GetMapping("")
     public ResponseEntity<?> fetchUserMealPlans(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok(userMealPlanService.findByUser(user));
+    }
+
+    @GetMapping("{mealPlanId}")
+    public ResponseEntity<?> fetchUserMealPlan(@PathVariable Long mealPlanId, @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(userMealPlanService.findMealsByMealPlanId(mealPlanId));
+
     }
 }
