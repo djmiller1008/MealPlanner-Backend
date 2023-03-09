67975ebbd27a1ecc4cfee791d8e0dacecd9968f1
David <djmiller1008@yahoo.com>
2023-03-09 23:51:35 +0800

    Add endpoint to create a new user meal

--- src/main/java/com/recipeapp/RecipeApp/web/UserMealController.java
+++ src/main/java/com/recipeapp/RecipeApp/web/UserMealController.java
@@ -10,16 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.recipeapp.RecipeApp.domain.UserMeal;
 import com.recipeapp.RecipeApp.service.UserMealService;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/user-meals")
 public class UserMealController {
 
     @Autowired
     private UserMealService userMealService;
-
+ 
     @PostMapping("")
-    public ResponseEntity<?> createUserMeal(@RequestBody String name) {
-        UserMeal newUserMeal = userMealService.createUserMeal( name);
+    public ResponseEntity<?> createUserMeal(@RequestBody Map<String, Object> payload) {
+        UserMeal newUserMeal = userMealService.createUserMeal(payload);
         return ResponseEntity.ok(newUserMeal);
     }
 }
