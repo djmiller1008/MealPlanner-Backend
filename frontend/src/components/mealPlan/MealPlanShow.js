5fdf01977bdff6418838697cbb6d369368d5bf11
David <djmiller1008@yahoo.com>
2023-03-09 23:58:12 +0800

    Display name of meal plan and meals, fetch info from backend

--- frontend/src/components/mealPlan/MealPlanShow.js
+++ frontend/src/components/mealPlan/MealPlanShow.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchUserMealPlanMeals } from '../util/ApiUtil';
+import { useLocalState } from '../util/LocalStorageUtil'
 
-export default function MealPlanShow() {
+export default function MealPlanShow(props) {
+  const [mealPlanData, setMealPlanData] = useLocalState(
+      props.location.state.mealPlan,
+      `mealPlan${props.location.state.mealPlan.id}`
+  );
+
+  const [meals, setMeals] = useState(null);
+
+  useEffect(() => {
+    fetchUserMealPlanMeals(mealPlanData.id).then(result => {
+      setMeals(result.data);
+    })
+  }, [])
+
+  
   return (
     <div>
-        Meal Plan Show Page 
+        <h1>{mealPlanData.name}</h1>
+        {meals ? meals.map(meal => <div>{meal.name}</div>) : ""}
     </div>
   )
 }
+ 
