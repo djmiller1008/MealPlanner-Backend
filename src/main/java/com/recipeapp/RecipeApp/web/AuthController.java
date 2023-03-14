9552087077b64765fccd6b9f4184ff3fe58167de
David <djmiller1008@yahoo.com>
2023-03-14 20:08:32 +0800

    Add endpoint for validating a jwt, add an error message in case of a BadCredentialsException

--- src/main/java/com/recipeapp/RecipeApp/web/AuthController.java
+++ src/main/java/com/recipeapp/RecipeApp/web/AuthController.java
@@ -7,11 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.jsonwebtoken.ExpiredJwtException;
 
 import com.recipeapp.RecipeApp.dto.AuthCredentialsRequest;
 import com.recipeapp.RecipeApp.util.JwtUtil;
@@ -47,7 +51,18 @@ public class AuthController {
                     user
                 );
         } catch (BadCredentialsException ex) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid Credentials");
         }
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<?> validateToken(@RequestParam String token, @AuthenticationPrincipal User user) {
+        try {
+            Boolean validated = jwtUtil.validateToken(token, user);
+            return ResponseEntity.ok(validated);
+        } catch (ExpiredJwtException e) {
+            return ResponseEntity.ok(false);
+        }
+      
     }
 }
