15cfed94d1a87a06cf99c35ae3b7d49253e20994
David <djmiller1008@yahoo.com>
2023-03-14 20:04:56 +0800

    Remove Auth Route, check if jwt is valid before rendering the given route

--- frontend/src/components/util/RouteUtil.js
+++ frontend/src/components/util/RouteUtil.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import { useLocalState } from './LocalStorageUtil';
+import React, { useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { validateJwtToken } from './ApiUtil';
 
-export const PrivateRoute = (props) => {
-    const [jwt, setJwt] = useLocalState("", "jwt");
-    return jwt ? (
-        <Route {...props} />
-    ) : (
-        <Redirect to="/login" />
-    );
+export const PrivateRoute = props => {
+    const jwt = JSON.parse(localStorage.getItem("jwt"));
+    const [isLoading, setIsLoading] = useState(true);
+    const [isValid, setIsValid] = useState(null);
+
+    if (jwt) {
+        validateJwtToken(jwt).then(result => {
+           setIsValid(result.data);
+           setIsLoading(false);
+        });
+    } else {
+        return <Redirect to="/login" />;
+    }
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    } else {
+        if (isValid) {
+            return <Route {...props} />;
+        } else {
+            return <Redirect to="/login" />;
+        }
+    }
 }
 
-export const AuthRoute = (props) => {
-    const [jwt, setJwt] = useLocalState("", "jwt");
-    return jwt ? (
-        <Redirect to="/" />
-    ) : (
-        <Route {...props} />
-    );
-}
