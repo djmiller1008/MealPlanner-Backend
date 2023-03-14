ad3735625ea6a73249e476636a6603326d66b6f0
David <djmiller1008@yahoo.com>
2023-03-14 20:07:39 +0800

    Change jwt expiration time back to 30 days

--- src/main/java/com/recipeapp/RecipeApp/util/JwtUtil.java
+++ src/main/java/com/recipeapp/RecipeApp/util/JwtUtil.java
@@ -21,7 +21,7 @@ public class JwtUtil implements Serializable {
 
     private static final long serialVersionUID = -2550185165626007488L;
 
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+    public static final long JWT_TOKEN_VALIDITY = 60 * 60 * 24 * 30;
 
     @Value("${jwt.secret}")
     private String secret;
