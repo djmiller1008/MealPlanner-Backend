cb7233698ffdd861e184daaa8075163be2e627ec
David <djmiller1008@yahoo.com>
2023-03-17 10:20:26 +0800

    Check if user is logged in and render accordingly

--- frontend/src/components/landing/HamburgerMenu.js
+++ frontend/src/components/landing/HamburgerMenu.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
 
-export default function HamburgerMenu() {
+export default function HamburgerMenu({ loggedIn }) {
+
+    const renderLogin = () => {
+        if (!loggedIn) {
+            return <NavLink className="menu-item" to={'/login'}>LOG IN</NavLink>;
+        }
+        return <NavLink className="menu-item" to={'/'}>DASHBOARD</NavLink>;
+    }
+
     return (
         <Menu>
             <i className="fa fa-cutlery logo sidebar-logo" aria-hidden="true"></i>
-            <NavLink className="menu-item" to={"/search"}>RECIPES</NavLink>
-            <NavLink className="menu-item" to={"/login"}>LOG IN</NavLink>
+            <NavLink className="menu-item" to={'/search'}>RECIPES</NavLink>
+            {renderLogin()}
         </Menu>
     )
 }
