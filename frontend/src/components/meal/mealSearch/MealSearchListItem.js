6280f40b0bea4cc76cca6a9eb27816c3871901ef
David <djmiller1008@yahoo.com>
2023-03-20 11:04:57 +0800

    Restructure search list item html

--- frontend/src/components/meal/mealSearch/MealSearchListItem.js
+++ frontend/src/components/meal/mealSearch/MealSearchListItem.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function MealSearchListItem({ recipe }) {
-
-
   return (
-    <div>
-        <Link to={`meal/${recipe.id}`}><h3>{recipe.title}</h3></Link>
-        <img src={recipe.image} alt={recipe.title}></img>
+    <div className='searchlist-item-div'>
+        <Link className='searchlist-item-link' to={`meal/${recipe.id}`}>
+          <img className='searchlist-item-image' src={recipe.image} alt={recipe.title}></img>
+          <section className='searchlist-item-text-section'>
+            <h3 className='searchlist-item-text'>{recipe.title}</h3>
+          </section>
+        
+        </Link>
     </div>
   )
 }
