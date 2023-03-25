1bae23d648103136429f51463f7683f98325ac8c
David <djmiller1008@yahoo.com>
2023-03-25 10:44:40 +0800

    Add React context to keep track of the state of the user

---
+++ frontend/src/components/userProvider/UserProvider.js
@@ -0,0 +1,21 @@
+import React, { createContext, useContext } from 'react';
+import { useLocalState } from '../util/LocalStorageUtil';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+    const [jwt, setJwt] = useLocalState("", "jwt");
+    const value = { jwt, setJwt }
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+};
+
+const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+      throw new Error("useUser must be used within a UserProvider");
+    }
+  
+    return context;
+  }
+
+export { useUser, UserProvider };
