514cf445ac68eb368d02baf3cdd47432cd0cc430
David <djmiller1008@yahoo.com>
2023-03-26 15:39:35 +0800

    Edit and add classes

--- frontend/src/components/mealPlan/CreateMealPlan.js
+++ frontend/src/components/mealPlan/CreateMealPlan.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import NavBar from '../landing/NavBar';
 import { createUserMealPlan } from '../util/ApiUtil';
 
 export default function CreateMealPlan() {
@@ -18,12 +19,17 @@ export default function CreateMealPlan() {
     }
     
     return (
-      <div>
-          <h1>Enter a Name For Your Meal Plan</h1>
-          <form onSubmit={(e) => handleSubmit(e)}>
-            <input onChange={(e) => handleInput(e)} type="text"></input>
-            <input type="submit" value="Create"></input>
-          </form>
-      </div>
+      <>
+        <NavBar />
+        <div className='new-meal-plan-container'>
+            <h1 className='mealplans-display-title'>Enter a Name For Your Meal Plan</h1>
+            <form className='create-mealplan-form' onSubmit={(e) => handleSubmit(e)}>
+              <section className='input-section'>
+                <input className='input' onChange={(e) => handleInput(e)} type="text"></input>
+              </section>
+              <input className='nav-button button' type="submit" value="Create"></input>
+            </form>
+        </div>
+      </>
     )
 }
