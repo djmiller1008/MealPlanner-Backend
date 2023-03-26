5b4082837eac898039b7625e4306c683cd99ec91
David <djmiller1008@yahoo.com>
2023-03-26 15:40:13 +0800

    Add new meal plan button, render a component for each mealplan

--- frontend/src/components/mealPlan/MealPlansDisplay.js
+++ frontend/src/components/mealPlan/MealPlansDisplay.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { fetchUserMealPlans } from '../util/ApiUtil';
-import { Link } from 'react-router-dom';
+import '../../styles/mealPlan.css';
+import MealPlanDisplayItem from './MealPlanDisplayItem';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function MealPlansDisplay() {
+    const history = useHistory();
     const [mealPlans, setMealPlans] = useState(null);
     
     useEffect(() => {
@@ -11,19 +14,21 @@ export default function MealPlansDisplay() {
         })
     }, []);
 
+
   return (
-    <div>
-        <h1>My Meal Plans</h1>
-        <div>
-            {mealPlans ? mealPlans.map((mealPlan, idx) => {
-                return (
-                    <div key={idx}>
-                        <Link to={`/mealPlan/${mealPlan.id}`}>
-                            {mealPlan.name}
-                        </Link>
-                    </div>
-                )})
-                : <></>}
+    <div className='mealplans-display-container'>
+        <div className='mealplans-content-container'>
+            <section className='mealplans-content-header'>
+                <h1 className='mealplans-display-title'>My Meal Plans</h1>
+                <Link to={'/create'} className='nav-button new-meal-plan-button'>Start a New Meal Plan</Link>
+            </section>
+            <div className='mealplan-items-container'>
+                {mealPlans ? mealPlans.map((mealPlan, idx) => {
+                    return (
+                        <MealPlanDisplayItem key={idx} mealPlan={mealPlan} />
+                    )})
+                    : <></>}
+            </div>
         </div>
     </div>
   )
