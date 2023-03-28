14ec8264bb56839db01a29e17b90039c10f277bc
David <djmiller1008@yahoo.com>
2023-03-28 14:58:56 +0800

    Refactor which parts of the item are clickable, add button to delete the meal

--- frontend/src/components/mealPlan/MealPlanMealItem.js
+++ frontend/src/components/mealPlan/MealPlanMealItem.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { deleteMealFromMealPlan } from '../util/ApiUtil';
 
-export default function MealPlanMealItem({ meal }) {
+export default function MealPlanMealItem({ mealPlanId, meal }) {
   const history = useHistory();
 
   const handleClick = () => {
     history.replace(`/meal/${meal.spoonacularId}`);
   }
 
+  const handleDelete = () => {
+    deleteMealFromMealPlan(mealPlanId, meal.id).then(response => {
+      if (response.status === 200) {
+        const item = document.getElementById(`meal-${meal.id}`);
+        item.style.display = 'none';
+      }
+    })
+  }
+
   return (
-    <div onClick={handleClick} className='mealplan-item-container'>
-        <img className='mealplan-item-image' src={meal.imageUrl} alt={meal.name} />
+    <div id={`meal-${meal.id}`} className='mealplan-item-container'>
+        <img onClick={handleClick} className='mealplan-item-image' src={meal.imageUrl} alt={meal.name} />
         <section className='mealplan-item-text-section'>
-            <h2>{meal.name}</h2>
+            <h2 className='mealplan-item-text' onClick={handleClick}>{meal.name}</h2>
+            <p onClick={handleDelete} className='delete-meal-button'>&times;</p>
         </section>
-        
     </div>
   )
 }
