10109a909dfaf83df04ca8b597be4707a47bbf8c
David <djmiller1008@yahoo.com>
2023-03-28 14:57:33 +0800

    Add delete endpoint

--- src/main/java/com/recipeapp/RecipeApp/web/UserMealController.java
+++ src/main/java/com/recipeapp/RecipeApp/web/UserMealController.java
@@ -1,7 +1,10 @@
 package com.recipeapp.RecipeApp.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +26,16 @@ public class UserMealController {
     public ResponseEntity<?> createUserMeal(@RequestBody Map<String, Object> payload) {
         UserMeal newUserMeal = userMealService.createUserMeal(payload);
         return ResponseEntity.ok(newUserMeal);
+    }
+
+    @DeleteMapping(value = "/{mealPlanId}/{mealId}")
+    public ResponseEntity<?> deleteMealFromMealPlan(@PathVariable Long mealId, @PathVariable Long mealPlanId) {
+        try {
+            userMealService.delete(mealId, mealPlanId);
+            return ResponseEntity.ok("Meal Deleted");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
