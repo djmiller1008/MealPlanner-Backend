12379bd08b8975630f949a263628af9addf568bc
David <djmiller1008@yahoo.com>
2023-03-28 15:00:03 +0800

    Pass mealplanid to each meal item

--- frontend/src/components/mealPlan/MealPlanShow.js
+++ frontend/src/components/mealPlan/MealPlanShow.js
@@ -4,33 +4,37 @@ import NavBar from '../landing/NavBar';
 import { fetchUserMealPlanMeals } from '../util/ApiUtil';
 import MealPlanMealItem from './MealPlanMealItem';
 
-
-export default function MealPlanShow(props) {
+export default function MealPlanShow() {
   const params = useParams();
 
   const [meals, setMeals] = useState(null);
-  const [mealPlanName, setMealPlanName] = useState("");
+  const [mealPlan, setMealPlan] = useState("");
 
   useEffect(() => {
     fetchUserMealPlanMeals(params.id).then(result => {
       setMeals(result.data.userMealsResponse);
-      setMealPlanName(result.data.userMealPlanResponse.name);
+      setMealPlan(result.data.userMealPlanResponse);
     })
   }, [])
+
+  const renderName = () => {
+    if (mealPlan.name) {
+      return mealPlan.name.slice(1, -1);
+    }
+    return "";
+  }
 
   return (
     <>
       <NavBar />
       <div className='mealplan-show-container'>
           <section className='mealplan-show-title-container'>
-            <h1 className='mealplans-display-title'>{mealPlanName.slice(1, -1)}</h1>
+            <h1 className='mealplans-display-title'>{renderName()}</h1>
             <Link className='nav-button mealplan-show-button' to={'/search'} >Add New Recipe</Link>
           </section>
-         
           <div className='meals-display-container'>
-            {meals ? meals.map((meal, idx) => <MealPlanMealItem key={idx} meal={meal} />) : ""}
+            {meals ? meals.map((meal, idx) => <MealPlanMealItem mealPlanId={mealPlan.id} key={idx} meal={meal} />) : ""}
           </div>
-          
       </div>
     </>
   )
