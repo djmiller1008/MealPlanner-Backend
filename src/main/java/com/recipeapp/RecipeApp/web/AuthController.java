0ac2dd088e00eb7e551caff481726bf0602e4c56
David <djmiller1008@yahoo.com>
2023-04-24 11:48:21 +0800

    Set the jwt in a cookie in the clients browser, get rid of the cookie on logout request

--- src/main/java/com/recipeapp/RecipeApp/web/AuthController.java
+++ src/main/java/com/recipeapp/RecipeApp/web/AuthController.java
@@ -2,23 +2,28 @@ package com.recipeapp.RecipeApp.web;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.jsonwebtoken.ExpiredJwtException;
 
 import com.recipeapp.RecipeApp.dto.AuthCredentialsRequest;
 import com.recipeapp.RecipeApp.util.JwtUtil;
+
+import ch.qos.logback.core.util.Duration;
+
 import com.recipeapp.RecipeApp.domain.User;
 
 @RestController
@@ -30,6 +35,9 @@ public class AuthController {
 
     @Autowired
     private JwtUtil jwtUtil;
+
+    @Value("${cookies.domain}")
+    private String domain;
     
     @PostMapping("login")
     public ResponseEntity<?> login (@RequestBody AuthCredentialsRequest request) {
@@ -42,13 +50,21 @@ public class AuthController {
             
             User user = (User) authenticate.getPrincipal();
             user.setPassword(null);
+
+            String token = jwtUtil.generateToken(user);
+            ResponseCookie cookie = ResponseCookie.from("jwt", token)
+                .domain(domain)
+                .path("/")
+                .maxAge(Duration.buildByDays(365).getMilliseconds())
+                .build();
+
             return ResponseEntity.ok()
                 .header(
-                    HttpHeaders.AUTHORIZATION,
-                    jwtUtil.generateToken(user)
+                    HttpHeaders.SET_COOKIE,
+                    cookie.toString()
                 )
                 .body(
-                    user
+                    token
                 );
         } catch (BadCredentialsException ex) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid Credentials");
@@ -56,7 +72,7 @@ public class AuthController {
     }
 
     @GetMapping("/validate")
-    public ResponseEntity<?> validateToken(@RequestParam String token, @AuthenticationPrincipal User user) {
+    public ResponseEntity<?> validateToken(@CookieValue(name = "jwt") String token, @AuthenticationPrincipal User user) {
         try {
             Boolean validated = jwtUtil.validateToken(token, user);
             return ResponseEntity.ok(validated);
@@ -65,4 +81,14 @@ public class AuthController {
         }
     }
 
+    @GetMapping("/logout")
+    public ResponseEntity<?> logout () {
+        ResponseCookie cookie = ResponseCookie.from("jwt", "")
+                .domain(domain)
+                .path("/")
+                .maxAge(0)
+                .build();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString()).body("ok");
+    }
 }
