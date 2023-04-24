f8dcfa4f53f75588994064c5353b1e5fa75fb70d
David <djmiller1008@yahoo.com>
2023-04-24 11:49:36 +0800

    Add UserMealItem route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import MealPlanShow from './components/mealPlan/MealPlanShow';
 import LandingPage from './components/landing/LandingPage';
 import { UserProvider } from './components/userProvider/UserProvider';
 import Register from './components/auth/Register';
+import UserMealItem from './components/meal/mealItem/UserMealItem';
 
 
 function App() { 
@@ -23,11 +24,12 @@ function App() {
           <Route exact path='/login' component={Login} />
           <Route exact path='/register' component={Register} />
           <PrivateRoute exact path="/" component={Dashboard} />
-          <Route exact path="/search" component={MealSearch} />
+          <Route exact path='/search' component={MealSearch} />
           <Route exact path='/meal/:id' component={MealItem} />
-          <PrivateRoute exact path="/create" component={CreateMealPlan} />
-          <PrivateRoute exact path="/addMeal" component={AddToMealPlan} />
-          <PrivateRoute exact path="/mealPlan/:id" component={MealPlanShow} />
+          <PrivateRoute exact path='/create' component={CreateMealPlan} />
+          <PrivateRoute exact path='/addMeal' component={AddToMealPlan} />
+          <PrivateRoute exact path='/mealPlan/:id' component={MealPlanShow} />
+          <PrivateRoute exact path='/meal/:mealPlanId/:mealId' component={UserMealItem} />
         </Switch>
       </UserProvider>
     </div>
