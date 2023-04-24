9294d8ad150b74ab79aa7cc494592b0a1deef886
David <djmiller1008@yahoo.com>
2023-04-24 11:48:44 +0800

    Change register endpoint to send back a cookie

--- src/main/java/com/recipeapp/RecipeApp/web/UserController.java
+++ src/main/java/com/recipeapp/RecipeApp/web/UserController.java
@@ -1,10 +1,12 @@
 package com.recipeapp.RecipeApp.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,6 +20,8 @@ import com.recipeapp.RecipeApp.dto.UserDto;
 import com.recipeapp.RecipeApp.service.UserService;
 import com.recipeapp.RecipeApp.util.JwtUtil;
 
+import ch.qos.logback.core.util.Duration;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -27,6 +31,9 @@ public class UserController {
 
     @Autowired
     private JwtUtil jwtUtil;
+
+    @Value("${cookies.domain}")
+    private String domain;
 
     @Autowired
     private UserService userService;
@@ -45,12 +52,21 @@ public class UserController {
 
                 User user = (User) authenticate.getPrincipal();
                 user.setPassword(null);
+                String token = jwtUtil.generateToken(user);
+                ResponseCookie cookie = ResponseCookie.from("jwt", token)
+                    .domain(domain)
+                    .path("/")
+                    .maxAge(Duration.buildByDays(365).getMilliseconds())
+                    .build();
+    
                 return ResponseEntity.ok()
                     .header(
-                        HttpHeaders.AUTHORIZATION,
-                        jwtUtil.generateToken(user)
+                        HttpHeaders.SET_COOKIE,
+                        cookie.toString()
                     )
-                    .body(user);
+                    .body(
+                        token
+                    );
         } catch (BadCredentialsException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
