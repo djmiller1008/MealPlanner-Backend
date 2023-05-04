c8a67dd36e2d1e33ed51727456a016ae1624d25f
David <djmiller1008@yahoo.com>
2023-05-04 11:29:31 +0800

    Add exception handler method to catch validation errors

--- src/main/java/com/recipeapp/RecipeApp/web/UserController.java
+++ src/main/java/com/recipeapp/RecipeApp/web/UserController.java
@@ -7,13 +7,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.http.ResponseCookie;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
 
 import com.recipeapp.RecipeApp.domain.User;
 import com.recipeapp.RecipeApp.dto.UserDto;
@@ -21,6 +24,10 @@ import com.recipeapp.RecipeApp.service.UserService;
 import com.recipeapp.RecipeApp.util.JwtUtil;
 
 import ch.qos.logback.core.util.Duration;
+import jakarta.validation.Valid;
+
+import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/users")
@@ -39,36 +46,45 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    private ResponseEntity<?> createUser(@RequestBody UserDto userDto) {
+    private ResponseEntity<?> createUser(@Valid @RequestBody UserDto userDto) {
         userService.createUser(userDto);
+       
+        Authentication authenticate = authenticationManager
+            .authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    userDto.getUsername(), userDto.getPassword()
+                )
+            );
 
-        try {
-            Authentication authenticate = authenticationManager
-                .authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                        userDto.getUsername(), userDto.getPassword()
-                    )
+            User user = (User) authenticate.getPrincipal();
+            user.setPassword(null);
+            String token = jwtUtil.generateToken(user);
+            ResponseCookie cookie = ResponseCookie.from("jwt", token)
+                .domain(domain)
+                .path("/")
+                .maxAge(Duration.buildByDays(365).getMilliseconds())
+                .build();
+
+            return ResponseEntity.ok()
+                .header(
+                    HttpHeaders.SET_COOKIE,
+                    cookie.toString()
+                )
+                .body(
+                    token
                 );
+        
+    }
 
-                User user = (User) authenticate.getPrincipal();
-                user.setPassword(null);
-                String token = jwtUtil.generateToken(user);
-                ResponseCookie cookie = ResponseCookie.from("jwt", token)
-                    .domain(domain)
-                    .path("/")
-                    .maxAge(Duration.buildByDays(365).getMilliseconds())
-                    .build();
-    
-                return ResponseEntity.ok()
-                    .header(
-                        HttpHeaders.SET_COOKIE,
-                        cookie.toString()
-                    )
-                    .body(
-                        token
-                    );
-        } catch (BadCredentialsException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 }
