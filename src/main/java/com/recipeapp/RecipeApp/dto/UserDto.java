43fbd7dd2ca30c2e9521d38a928d45c648e8926d
David <djmiller1008@yahoo.com>
2023-05-04 11:30:39 +0800

    Add not blank validations and unique constraint to user dto username and password

--- src/main/java/com/recipeapp/RecipeApp/dto/UserDto.java
+++ src/main/java/com/recipeapp/RecipeApp/dto/UserDto.java
@@ -2,10 +2,18 @@ package com.recipeapp.RecipeApp.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserDto {
+
+    @Column(unique=true)
     @JsonProperty("username")
+    @NotBlank(message = "Please Enter a Username")
     private String username;
 
+
+    @NotBlank(message = "Please Enter a Password")
     @JsonProperty("password")
     private String password;
 
